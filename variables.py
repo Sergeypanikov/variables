4433ca07ec2d266c6e01a46b1456461e2d50b8f4
Sergeypanikov <svp1967@gmail.com>
2024-11-28 11:28:48 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,9 @@
+number_of_homework_assignments_completed = "12"
+number_of_hours_spent = "1.5"
+course_name = "Python"
+time_for_one_task = "12 / 1.5"
+print("course_name:" + "Python")
+print("number_of_homework_assignments_completed:" + "12")
+print("number_of_hours_spent:" + "1.5")
+print("time_for_one_task:")
+print(12.0 / 1.5)
